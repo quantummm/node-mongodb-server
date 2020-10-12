3b7f3f545c7f033597774e26861adcf35268f368
Rongzhi Wang <aidenwang0826@gmail.com>
2020-10-12 14:38:42 +1100

    add zodiac and routes

--- src/routes/user/index.js
+++ src/routes/user/index.js
@@ -3,11 +3,13 @@ import { Router } from "express";
 import register from "./register";
 import login from "./login";
 import profile from "./profile";
+import zodiac from "./zodiac";
 
 const routes = Router();
 
 routes.use(register);
 routes.use(login);
 routes.use(profile);
+routes.use(zodiac);
 
 export default routes;
